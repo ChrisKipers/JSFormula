a583217d7a5d31cbf3933d3e3294fdef26d15758
Chris Kipers <ckipers@gmail.com>
2014-01-11 11:29:46 -0800

    First overhall complete

--- functionprocessor.js
+++ functionprocessor.js
@@ -1,220 +1,317 @@
 var FunctionProcessor = (function() {
     var functions = {},
+    	cache = {},
+    	FUNC = 1,
+		VAR = 2,
+		LIT = 3,
+		OPP = 4,
 		presedence = {
-			'+' : 1,
-			'-' : 1,
-			'*' : 2,
-			'/' : 2,
-			'^' : 3
+			'+' : {
+				presendence : 1,
+				func: function (a, b) {
+					return a + b;
+				},
+				arguments: 2,
+				type: OPP
+			},
+			'-' : {
+				presendence : 1,
+				func: function (a, b) {
+					return a - b;
+				},
+				arguments: 2,
+				type: OPP
+			},
+			'*' : {
+				presendence : 2,
+				func: function (a, b) {
+					return a * b;
+				},
+				arguments: 2,
+				type: OPP
+			},
+			'/' : {
+				presendence : 2,
+				func: function (a, b) {
+					return a / b;
+				},
+				arguments: 2,
+				type: OPP
+			},
+			'^' : {
+				presendence : 3,
+				func: function (a, b) {
+					return MATH.pow(a, b);
+				},
+				arguments: 2,
+				type: OPP
+			}
 		},
-		standardTokens = [
-			'+',
-			'-',
-			'*',
-			'/',
-			'^'
-		],
-		tokenize = function(input, possibleTokens, object) {
-			var objKeys = Object.keys(object);
-			var curIndex = 0;
-			var tokens = [];
-			var allTokenBlocks = [];
-			while (curIndex < input.length) {
+		variableOpen = '{',
+    	variableEnd = '}',
+    	variableRegex = new RegExp('^' + variableOpen + '.*' + variableEnd),
+		functionRegex = null,
+		opporatorRegex = /^\+|^-|^\\|^\*|^\^/,
+		tokenize = function(input) {
+			var curIndex = 0,
+				tokens = [],
+				token,
+				regexMatch,
+				curRemainingString,
+				inputLen = input.length,
+				firstChar,
+				nextMatPosition,
+				funcName,
+				paramsUnparsed,
+				params,
+				stripFirstParam,
+				opperator;
+
+			while (curIndex < inputLen) {
+				curRemainingString = input.substring(curIndex).trim();
 				//This part of the code check for strings
-				var firstChar = input.substring(curIndex, curIndex + 1);
+				firstChar = input.substring(curIndex, curIndex + 1);
 				if (firstChar == '"' || firstChar == "'") {
-					var remanderString = input.substring(curIndex + 1, input.length);
-					var nextQuotePosition = remanderString.indexOf(firstChar)
-					if (nextQuotePosition == -1) {
+					nextMatPosition = input.indexOf(firstChar,curIndex + 1);
+					if (nextMatPosition == -1) {
 						throw "Quote miss match";
 					} else {
-						tokens.push(input.substring(curIndex,curIndex + nextQuotePosition + 2));
-						curIndex += curIndex + nextQuotePosition + 2;
+						tokens.push({
+							type: LIT,
+							val : input.substring(curIndex,curIndex + nextMatPosition + 2)
+						});
+						curIndex += curIndex + nextMatPosition + 2;
 						continue;
 					}
-				} else if(firstChar == ',') { //This part of the code check for commas
-
-					var subTokenBlocks = tokenize(input.substring(curIndex + 1, input.length), possibleTokens, functions);
-					subTokenBlocks.forEach(function(tokenBlock) {
-						allTokenBlocks.push(tokenBlock)
+				}else if (regexMatch = getFirstRegex(variableRegex, curRemainingString)) {
+					regexMatch = regexMatch.remove(variableOpen).remove(variableEnd);
+					tokens.push({
+						type: VAR,
+						val: regexMatch
 					});
+					curIndex = curIndex + 1 + regexMatch.length;
 					break;
-				}
-				//If the first character is not a quote or a comma we then determine what token it is
-				var curTokenIndex = -1;
-				for (var i = curIndex + 1; i <= input.length; i++) {
-					var token = input.substring(curIndex, i);
-					var newTokenIndex = possibleTokens.indexOf(token);
-					if (newTokenIndex != -1 || (!isNaN(token.trim()) && token.trim().length != 0)) {
-						curTokenIndex = i
-					} else if (!possibleMatch(token, possibleTokens)) {
-						break;
+				} else if (regexMatch = getFirstRegex(functionRegex,curRemainingString)) {
+					nextMatPosition = regexMatch.indexOf('(');
+					funcName = regexMatch.substring(0,nextMatPosition);
+					stripFirstParam = regexMatch.substring(nextMatPosition);
+					paramsUnparsed = findParameters(stripFirstParam.substring(1, stripFirstParam.length - 1));
+					params = [];
+					for (var i = 0, max = paramsUnparsed.length; i < max; i++) {
+						params.push(tokenize(paramsUnparsed[i], functions));
 					}
-				}
-
-				//If the value is not a token we just continue silently
-				if (curTokenIndex == -1) {
-					curIndex++;
+					tokens.push({
+						type: FUNC,
+						func: functions[funcName],
+						params: params
+					});
+					curIndex = curIndex + 1 + regexMatch.length;
+					break;
+				} else if (regexMatch = getFirstRegex(opporatorRegex, curRemainingString)) {
+					opperator = presedence[regexMatch];
+					tokens.push(opperator);
+					curIndex = curIndex + 1 + regexMatch.length;
 				} else {
-					var token = input.substring(curIndex, curTokenIndex);
-					//If the token is a number we cast it to a number and push it to the token stack
-					if (!isNaN(token)) {
-						tokens.push(parseFloat(token))
-					} else if(contains(functions,token)) { //If the token is a function we perform extra processing
-						var closingParamIndex = findNextClosingChar(input, curTokenIndex + 1, '(', ')');
-						//Process the inside of the parathesis to get the parameter values
-						var parameters = processPostFix(infixToPostFix(input.substring(curTokenIndex + 1,closingParamIndex), possibleTokens, object));
-						//Concatinate the parameter arrays into one array
-						var concatinatedParameters = [];
-						for (var i = 0; i < parameters.length; i++) {
-							concatinatedParameters.push(parameters[i][0])
-						}
-						//Calculate the results of the function and push them onto the stack
-						var result = functions[token].apply(this,concatinatedParameters);
-						tokens.push(result);
-						curTokenIndex = closingParamIndex;
-					} else if(objKeys.indexOf(token) != -1) {
-						var fieldData = processesFieldInfo(object,token,input, curTokenIndex);
-						tokens.push(fieldData);
+					nextMatPosition = curRemainingString.indexOf(' ');
+					if (nextMatPosition === -1) {
+						token = curRemainingString;
+						nextMatPosition = token.length;
 					} else {
-						tokens.push(token);
+						token = curRemainingString.substring(0, nextMatPosition);
 					}
-					curIndex = curTokenIndex
+					
+					if (isNaN(token)) {
+						throw "This is not an acceptable token: " + token;
+					}
+					tokens.push({
+						type: LIT,
+						val: new Number(token)
+					});
+					curIndex = curIndex + 1 + nextMatPosition;
 				}
 			}
-			allTokenBlocks.splice(0,0,tokens);
-			return allTokenBlocks;
+			return tokens;
 		},
-		infixToPostFix = function(infix, possibleTokens, object) {
-			var postFixs = [],
-				tokenBlocks = tokenize(infix, possibleTokens, object);
-			tokenBlocks.forEach(function(tokenBlock) {
-				var stack = [],
-					postFix = [];
-				tokenBlock.forEach(function(token) {
-					if (contains(presedence, token)) {
-						var peakChar = peek(stack)
-						if (peakChar == null) {
-							stack.push(token);
-						} else {
-							while (peek(stack) != null && presedence[peek(stack)] >= presedence[token]) {
-								postFix.push(stack.pop());
-							}
-							stack.push(token)
-						}
-					} else {
-						postFix.push(token);
-					}
-				})
-				while (stack.length > 0) {
-					postFix.push(stack.pop());
+		getFirstRegex = function(regex,string) {
+			if (regex) {
+				var results = regex.exec(string);
+				if(results) {
+					return results[0];
+				} else {
+					return results;
 				}
-				postFixs.push(postFix);
-			});
-			return postFixs
-		},
-		processPostFix = function(tokenBlocks) {
-			tokenBlocks.forEach(function(tokens){
-				for (var i = 0; i < tokens.length; i++) {
-					if (standardTokens.indexOf(tokens[i]) != -1) {
-						(function(){
-							var opperator = tokens[i],
-								firstOpperand = tokens[i - 2],
-								secondOpperand = tokens[i - 1],
-								calculated;
-							if (opperator == '+') {
-								calculated = firstOpperand + secondOpperand;
-							} else if (opperator == '-') {
-								calculated = firstOpperand - secondOpperand;
-							} else if (opperator == '*') {
-								calculated = firstOpperand * secondOpperand;
-							} else if (opperator == '/') {
-								calculated = firstOpperand / secondOpperand;
-							} else if (opperator == '^') {
-								calculated = Math.pow(firstOpperand,secondOpperand);
-							}
-							tokens.splice(i - 2, 3, calculated);
-							i = i - 2
-						}());
-					}
-				}
-			})
+			} else {
+				return null;
+			}
 			
-			return tokenBlocks;
 		},
-		evaluate = function(formula, object) {
-			object = object ? object : {}
-			var allPosibleTokens = [],
-				funcKeys = Object.keys(functions),
-				objKeys = Object.keys(object),
-				postfix;
+		findParameters = function(input) {
+			var curPos = 0,
+				params = [],
+				unclosedParams = 0,
+				curChar,
+				len = input.length;
 
-			standardTokens.forEach(function(token){
-				allPosibleTokens.push(token)
-			});
-			funcKeys.forEach(function(key) {
-				allPosibleTokens.push(key);
-			});
-			objKeys.forEach(function(key){
-				allPosibleTokens.push(key);
-			});
-			postfix = infixToPostFix(formula, allPosibleTokens, object);
-			return processPostFix(postfix)[0][0];
+			while(curPos < len) {
+				curChar = input[curPos];
+				if (curChar === '(') {
+					unclosedParams++;
+					curPos++;
+				} else if(curChar === ')') {
+					unclosedParams--;
+					if (unclosedParams < 0) {
+						throw "whaaa?"
+					}
+					curPos++;
+				} else if (curChar === ',' && unclosedParams == 0) {
+					params.push(input.substring(0,curPos));
+					input = input.substring(curPos + 1);
+					len = input.length;
+				} else {
+					curPos++;
+				}
+			}
+			params.push(input);
+			return params;
+		},
+		infixToPostFix = function(tokens) {
+			var stack =[],
+				postFix = [],
+				curToken,
+				topOpperator,
+				curStackLength;
+			for (var i = 0, max = tokens.length; i < max; i++) {
+				curToken = tokens[i];
+				if (curToken.type !== OPP) {
+					postFix.push(curToken);
+				} else {
+					if (stack.length === 0) {
+						stack.push(curToken);
+					} else {
+						topOpperator = stack[stack.length -1];
+						while (topOpperator && topOpperator.presedence >= curToken.presedence) {
+							postFix.push(stack.pop());
+							curStackLength = stack.length;
+							if (curStackLength > 0) {
+								topOpperator = stack[stack.length -1];
+							} else {
+								topOpperator = null;
+							}
+						}
+						stack.push(curToken);
+					}
+				}
+			}
+			while(curToken = stack.pop()) {
+				postFix.push(curToken);
+			}
+			return postFix;
+		},
+		buildTree = function(postFix) {
+			var i = 0,
+				max = postFix.length,
+				postFix = postFix.slice(0), //Copy it as to not modify the original array
+				currentToken,
+				numArgs,
+				params; 
+
+			while (i < max) {
+				currentToken = postFix[i];
+				if (currentToken.type !== OPP) {
+					i++;
+				} else {
+					params = [];
+					numArgs = currentToken.arguments;
+					while (numArgs > 0) {
+						try {
+							params.push(postFix[i - numArgs]);
+							numArgs--;
+						} catch (err) {
+							throw 'Malformed Expression';
+						}
+					}
+					postFix.splice(i - currentToken.arguments, currentToken.arguments + 1, {
+						type: FUNC,
+						params: params,
+						func: currentToken.func
+					});
+					i = i - 1;
+					max = postFix.length;
+				}
+			}
+			if (postFix.length !== 1) {
+				throw 'Malformed Expression';
+			}
+			return postFix[0];
+		},
+		eval = function(tree, object) {
+			if (tree.type === FUNC) {
+				var paramResults = [];
+				for (var i = 0, max = tree.params.length; i < max; i++) {
+					paramResults.push(eval(tree.params[i],object));
+				}
+				return tree.func.apply(this, paramResults);
+			} else if (tree.type === VAR) {
+				return evalVar(tree.val, object);
+			} else if (tree.type === LIT) {
+				return tree.val;
+			}
+		},
+		evalVar = function (variable, object) {
+			var path = variable.split('.');
+			var curObject = object;
+			for (var i = 0, max = path.length; i < max; i++) {
+				curObject = curObject[path[i]];
+			}
+			return curObject;
+		},
+		buildTreeFromScratch = function(input) {
+			var tokens = tokenize(input),
+				postFix;
+			postFix = infixToPostFix(tokens);
+			return buildTree(postFix);
+
+		},
+		calculate = function(input, object) {
+			var tree;
+
+			if (tree = cache[input]) {
+				return eval(tree, object);
+			} else {
+				tree = buildTreeFromScratch(input);
+				cache[input] = tree;
+			}
+			return eval(tree, object);
 		},
 		setFunctions = function(newFunctions) {
+			var functionRegexs = [],
+				regexString;
 			functions = newFunctions;
+			for (var prop in functions) {
+				if (functions.hasOwnProperty(prop)) {
+					functionRegexs.push('^' + prop + '\\(.*\\)');
+				}
+			}
+			if (!functionRegexs) {
+				functionRegex = null;
+			} else {
+				regexString = functionRegexs.join('|');
+				functionRegex = new RegExp(regexString);
+			}
 			return this;
 		},
-		contains = function(map, charactor) {
-			return Object.keys(map).indexOf(charactor) != -1;
-		},
-		findNextClosingChar= function(input, start,openChar,closingChar) {
-			var numberOfOpen = 1;
-			for (var i = start, max = input.length; i < max; i++) {
-				if (input[i] == openChar) {
-					numberOfOpen++;
-				} else if (input[i] == closingChar) {
-					numberOfOpen--;
-					if (numberOfOpen == 0) {
-						return i;
-					}
-				}
-			}
-			throw "No closing parameter found";
-		},
-		possibleMatch = function(token, possibleTokens) {
-			for (var i = 0, max = possibleTokens.length; i < max; i++) {
-				if (possibleTokens[i].indexOf(token) != -1) {
-					return true;
-				}
-			}
-			return false;
-		},
-		processesFieldInfo = function(object,token,input,end) {
-			var max = input.length,
-				endingBracket,
-				inside,
-				processedKey;
-
-			if (end + 1 < max && input[end] == '[') {
-				endingBracket = findNextClosingChar(input, end + 1, '[', ']')
-				inside = input.substring(end + 1, end + endingBracket - 1);
-				processedKey = evaluate(inside,object[token])
-				return processedKey;
+		setVarDelimiter = function(open, close) {
+			if (open && close) {
+				variableOpen = open;
+    			variableEnd = close;
+    			variableRegex = new RegExp('^' + variableOpen + '.*' + variableEnd);
 			} else {
-				return object[token]
-			}
-		},
-		peek = function(array) {
-			if (array.length == 0) {
-				return null;
-			} else {
-				return array[array.length -1]
+				variableRegex = null;
 			}
 		}
 
 	return {
 		"setFunctions" : setFunctions,
-		"evaluate" : evaluate
+		"calculate" : calculate
 	};
 }());
